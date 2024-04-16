a19106e1add2c62f6a7eaeb105cf85206fffb642
Zadock Maloba <malobazadock@gmail.com>
2024-04-16 22:05:24 +0300

    Use NavigationStackLayout to switch views

--- Objective-C-proj2/ContentView.swift
+++ Objective-C-proj2/ContentView.swift
@@ -8,21 +8,40 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var isShowingSecondView = false
+    
+    var body: some View {
+        NavigationStack {
+            VStack {
+                Image(systemName: "globe")
+                    .imageScale(.large)
+                    .foregroundStyle(.tint)
+                Text("Hello, world!")
+                
+                Button("Press"){
+                    NSLog(String(validatingUTF8: test_api_hello())!)
+                    
+                    var temp = TestObject()
+                    temp.doSomething()
+                }
+                NavigationLink(destination: SecondView(), isActive: $isShowingSecondView) {
+                                    Text("Go to Second View")
+                                }
+            
+            }
+            .padding()
+            .navigationTitle("First View")
+        }
+    }
+}
+
+struct SecondView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-            
-            Button(String(validatingUTF8: test_api_hello())!){
-                NSLog(String(validatingUTF8: test_api_hello())!)
-                
-                var temp = TestObject()
-                temp.doSomething()
-            }
+            Text("Second View")
         }
-        .padding()
+        .navigationTitle("Second View")
     }
 }
 
